d46a6616e6f62bf0b7da1f52e23ba4368802101f
Gabriel Roger <gabriel.nroger@gmail.com>
2020-07-29 18:21:12 -0300

    Create models of application

---
+++ app/blog/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(128))
+    title = db.Column(db.String(255))
+    description = db.Column(db.Text)
+    publication_date = db.Column(db.Date)
---
+++ app/blog/schemas.py
@@ -0,0 +1,15 @@
+from marshmallow import Schema, fields, post_load
+
+from app.blog.models import Post
+
+
+class PostSchema(Schema):
+    id = fields.Integer(required=False, dump_only=True)
+    author = fields.String(required=True)
+    title = fields.String(required=True)
+    description = fields.String(required=True)
+    publication_date = fields.Date(required=True)
+
+    @post_load
+    def make_post(self, data, **kwargs):
+        return Post(**data)
